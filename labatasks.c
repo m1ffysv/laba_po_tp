8f8e449851cdc37ceb9ec0427f6e34b898e5ef3d
m1ffysv <mamidudin@edu.hse.ru>
2025-10-06 22:00:32 +0300

    added poeration

--- labatasks.c
+++ labatasks.c
@@ -8,6 +8,9 @@ int main(){
     printf("Введите число A, затем число B:\n");
     scanf("%d", &numb1);
     scanf("%d", &numb2);
+    int plus;
+    plus = numb1 + numb2;
+    printf("%d + %d = %d\n", numb1, numb2, plus);
     return 0;
 
 
