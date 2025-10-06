3af8d90f0cd15cf5054164698e73183acdfca741
m1ffysv <mamidudin@edu.hse.ru>
2025-10-06 21:45:58 +0300

    Update labatasks.c

--- labatasks.c
+++ labatasks.c
@@ -2,8 +2,14 @@
 
 int main(){
 
-    printf("Hello, world!");
+    int numb1;
+    int numb2;
+    printf("Hello, world!\n");
+    printf("Введите число A, затем число B:\n");
+    scanf("%d", &numb1);
+    scanf("%d", &numb2);
     return 0;
+
 
 
 }
